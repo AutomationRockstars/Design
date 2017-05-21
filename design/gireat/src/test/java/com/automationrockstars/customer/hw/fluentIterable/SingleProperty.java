ad502ef6f091a4d6c4c4f4a2d9be3c293f361b25
Admin AutomationRockstars <admin@automationrockstars.com>
2017-05-21 22:59:56 +0100

    removing redundant tests

--- design/gireat/src/test/java/com/automationrockstars/customer/hw/fluentIterable/SingleProperty.java
+++
@@ -1,9 +0,0 @@
-package com.automationrockstars.customer.hw.fluentIterable;
-
-import com.automationrockstars.gir.ui.FindBy;
-import com.automationrockstars.gir.ui.WithFindByAugmenter;
-
-@WithFindByAugmenter(PropertyAugmenter.class)
-@FindBy(id = "searchResults_propertyId")
-public interface SingleProperty extends Property {
-}
