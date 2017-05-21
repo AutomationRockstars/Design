01dc262d6b3c3d2f0770dd68d2028f8c72931f5b
Admin AutomationRockstars <admin@automationrockstars.com>
2017-05-21 20:42:07 +0100

    Setting phantom webdriver variable

--- design/gir-webdriver/src/main/java/com/automationrockstars/design/gir/webdriver/HeadlessWebDriver.java
+++ design/gir-webdriver/src/main/java/com/automationrockstars/design/gir/webdriver/HeadlessWebDriver.java
@@ -1,21 +1,29 @@
 package com.automationrockstars.design.gir.webdriver;
 
 import org.apache.commons.lang3.SystemUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HeadlessWebDriver {
 
+	private static final Logger LOG = LoggerFactory.getLogger(HeadlessWebDriver.class);
+			
 	public static String name(){
+		String broser = "";
 		try {
+			LOG.info("Detectong phantoom version");
 			if (SystemUtils.IS_OS_WINDOWS){
 				new ProcessBuilder("cmd","/C","phantomjs", "--version").inheritIO().start().waitFor();
 			} else {
 				new ProcessBuilder("bash","phantomjs","--version").inheritIO().start().waitFor();
 			}
 			System.setProperty("webdriver.phantomjs.driver", "phantomjs");
-			return "phantomjs";
+			broser = "phantomjs";
 		} catch (Throwable t){
-			t.printStackTrace();
-			return "htmlUnit";
+			LOG.debug("Phantomjs detection failed due to {}",t);
+			broser = "htmlUnit";
 		}
+		LOG.info("Found headless browser {}",broser);
+		return broser;
 	}
 }
