a033822f98a6ced572cd83ced6732ab74e643595
Admin AutomationRockstars <admin@automationrockstars.com>
2017-05-21 20:39:33 +0100

    Setting phantom webdriver variable

--- design/gir-webdriver/src/main/java/com/automationrockstars/design/gir/webdriver/HeadlessWebDriver.java
+++ design/gir-webdriver/src/main/java/com/automationrockstars/design/gir/webdriver/HeadlessWebDriver.java
@@ -2,8 +2,6 @@ package com.automationrockstars.design.gir.webdriver;
 
 import org.apache.commons.lang3.SystemUtils;
 
-import com.automationrockstars.base.ConfigLoader;
-
 public class HeadlessWebDriver {
 
 	public static String name(){
@@ -13,7 +11,7 @@ public class HeadlessWebDriver {
 			} else {
 				new ProcessBuilder("bash","phantomjs","--version").inheritIO().start().waitFor();
 			}
-			ConfigLoader.config().addProperty("webdriver.phantomjs.driver", "phantomjs");
+			System.setProperty("webdriver.phantomjs.driver", "phantomjs");
 			return "phantomjs";
 		} catch (Throwable t){
 			t.printStackTrace();
