f171723d1d86c3706343e5402c811835c78238cb
Rafal Kosmulski <Rafal.X.Kosmulski@aib.ie>
2017-10-02 16:50:24 +0100

    Adding permutator in test data pool - initial revision

--- design/gir-data/src/main/java/com/automationrockstars/gir/data/TestDataPool.java
+++ design/gir-data/src/main/java/com/automationrockstars/gir/data/TestDataPool.java
@@ -1,5 +1,8 @@
 package com.automationrockstars.gir.data;
 
+import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+
 public interface TestDataPool {
 
 	String poolName();
@@ -7,6 +10,13 @@ public interface TestDataPool {
 	void close();
 	<T extends TestDataRecord> TestData<T> testData(Class<T> clazz);
 	<T extends TestDataRecord> TestData<T> exclusiveTestData(Class<T> clazz);
-	
+
+	static interface TestDataPermutator<T extends TestDataRecord> {
+	    TestData<T> build(Class<T> recordType);
+        TestData<T> buildExclusive(Class<T> recordType);
+        <V extends TestDataRecord> TestDataPermutator<T> combine(Class<V>... recordTypes);
+        TestDataPermutator<T> exclude(Predicate<T> predicate);
+    }
+
 	
 }
